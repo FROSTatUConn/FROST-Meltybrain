dde9ed1fa1b9217e06ab328ac502486ed0d560a1
MiseryFell <natalievreeland@outlook.com>
2025-02-17 18:08:23 -0500

    continued work on read/write code

--- read_testing/read_testing.ino
+++ read_testing/read_testing.ino
@@ -3,10 +3,14 @@
 short output[3];
 
 void readMemory() {
-  for (int i = 0; i < 512; i+=6) {
+
+/* Iterates through the maximum number of arrays of three shorts that
+can be iterated through in the Arduino Nano's 1024 byte EEPROM */
+
+  for (int i = 0; i < 341; i+=6) {
     EEPROM.get(i, output);
-    for (int j = 0; i < 3; i++) {
-      Serial.print(output[j]);
+    for (int j = 0; j < 3; j++) {
+      Serial.print(output[j] + " ");
     }
     Serial.print('\n');
   }
--- writeMemory.ino
+++ writeMemory.ino
@@ -3,20 +3,31 @@ int prev = 0;
 short initial;
 
 void writeMemory() {
+  // Delays ONE MINUTE!!!
   delay(60000);
+
+  // Reads the first byte
   initial = EEPROM.read(0);
+
+  // This array of size three stores the current filtered coordinates
   short coords[3];
-  for (int j = 0; j < 3; j++) {
-    coords[j] = static_cast<short>(smooth[j]);
-  }
+
+  // 
   if (initial != 0) {
     int i = 6;
     EEPROM.put(0, static_cast<short>(0));
     while (time < 240000) {
-      if (((time - prev) >= 500)) {
+      unsigned int current_time = time;
+      if (((current_time - prev) >= 500)) {
         EEPROM.put(i, coords);
         i += 6;
       }
+      // This loop records the current three coordinates
+      for (int j = 0; j < 3; j++) {
+        coords[j] = static_cast<short>(smooth[j]);
+      }
+
+
     }
   }
 }
